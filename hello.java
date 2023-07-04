c10654508ec9923c3758d576418755acf4749c10
babithasiddula <138086027+babithasiddula@users.noreply.github.com>
2023-07-04 22:36:35 +0530

    Update hello.java
    Updating in remote repo

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class main()
 {
 public static void main(String args[])
 {
-System.out.println("Hello child branch");
+System.out.println("Editing child branch hello.java file from remote repo");
 }
 }
